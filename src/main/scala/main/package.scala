7a898cb49d3fe64990a466512c8c85b839b02812
nik_01 <nik@gmail.com>
2021-03-19 14:14:26 +0100

    added dict to remove duplicate

--- src/main/scala/main/package.scala
+++ src/main/scala/main/package.scala
@@ -9,10 +9,10 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 import java.nio.file.{Files, Path, Paths}
+import scala.collection.immutable.HashMap
 //added comment
 object main extends App {
-  System.setProperty("http.proxyHost", "proxy-se-uan.ddc.teliasonera.net")
-  System.setProperty("http.proxyPort", "8080")
+
 
   def getLinks(url: String, selector: String,rootUrl: String) = {
     val document: Document = Jsoup.connect(url).get()
@@ -20,28 +20,36 @@ object main extends App {
     val links = for(aTag <- aTags.asScala) yield aTag.attr("href")
     links.toIndexedSeq
     val loadableLinks = for(link <- links) yield if(!(link.startsWith("www.")||link.startsWith("http")||link.startsWith("https") || link.equals("") || link.isEmpty))rootUrl + link
-    loadableLinks.toList
+    loadableLinks.toList.map(x => x.toString)
   }
 
 
-  def downloadFiles(downloadableFileLinks: List[Any], path: String, selector: String, rootUrl:String): Unit = {
-    downloadableFileLinks.map( downloadableLink =>
+
+
+
+
+
+  def downloadFiles2(downloadableFileLinks: scala.collection.mutable.Map[String, Boolean], path: String, selector: String, rootUrl:String): Unit = {
+    if(downloadableFileLinks.filter(x => x._2==false ).size==0){
+      return
+    }
+    downloadableFileLinks.filter(x => x._2==false ).keys
+      .filter(x => x.startsWith("https://tretton37.com/")).foreach(downloadableLink =>
     {
-      // TODO: Only write if fileNotExists?
-      if(downloadableLink.toString.startsWith("https://tretton37.com/")) {
-        println("link is " + downloadableLink)
+
         val urlObject = new URL(downloadableLink.toString)
         val getElement = urlObject.getPath().replaceAll("/", "")
-        val directory = createDirIfNotExist(path + "\\" + getElement)
-        val filePath = (directory + "\\" + getElement + ".html")
-        urlObject #> new File(filePath) !!;
-        println("writing data into file" + filePath)
-        if (!path.contains(getElement))
-          downloadFiles(getLinks(urlObject.toString, selector, rootUrl), directory.toString, selector, rootUrl)
-      }
+
+
+        println("urlObject is " + urlObject)
+        downloadableFileLinks(urlObject.toString)=true
+        val tempMap = getHashMap(urlObject.toString, selector, rootUrl)
+        val newVal = tempMap.keySet.diff(downloadableFileLinks.keySet)
+      newVal.map(x => downloadableFileLinks += (x -> false))
+
+
     })
   }
-
   def createDirIfNotExist(dir: String): java.nio.file.Path = {
     val path = Paths.get(dir)
     if (Files.exists(path)) {
@@ -58,7 +66,17 @@ object main extends App {
   val rootUrl = "https://tretton37.com"
   val scrapedUrl = "https://tretton37.com/"
   val aLinksContentSelectpr = "li a[target !=_blank]"
-  val linksOnPage = getLinks(scrapedUrl, aLinksContentSelectpr, rootUrl)
-  downloadFiles(linksOnPage, "C:\\Users\\ose414\\IdeaProjects\\files\\Homepage", aLinksContentSelectpr, rootUrl)
+  val hashMapName = getHashMap(scrapedUrl,aLinksContentSelectpr, rootUrl)
 
+  downloadFiles2(hashMapName, "resource/Homepage", aLinksContentSelectpr, rootUrl)
+
+
+
+
+  def getHashMap(scrapedUrl:String,aLinksContentSelectpr:String, rootUrl:String) ={
+    val linksOnPage = getLinks(scrapedUrl, aLinksContentSelectpr, rootUrl)
+    var hashMapName = scala.collection.mutable.Map[String, Boolean]()
+     linksOnPage.map(x => hashMapName += (x -> false))
+    hashMapName
+  }
 }
